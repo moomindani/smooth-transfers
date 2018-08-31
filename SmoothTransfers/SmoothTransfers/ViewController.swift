34244eb6516339635fa4ee3ef555d32141087a04
moomindani <moomindani@gmail.com>
2018-08-31 10:33:08 +0900

    Initial Commit

---
+++ SmoothTransfers/SmoothTransfers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmoothTransfers
+//
+//  Created by Noritaka Sekiyama on 2018/08/31.
+//  Copyright © 2018年 Noritaka Sekiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
